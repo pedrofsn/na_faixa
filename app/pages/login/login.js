616b6ab24062e8d8d40b6320bc58ecc89604652e
PEDRO FRANCISCO DE SOUSA NETO <pedro.francisco@cit.local>
2016-03-28 17:38:11 -0300

    adicionando oauth com google

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -1,5 +1,8 @@
 import {Page, Alert, NavController} from 'ionic-angular';
 import {HomePage} from '../home/home';
+import {Http} from 'angular2/http';
+import {provide} from 'angular2/core';
+import {CordovaOauth, Google} from 'ng2-cordova-oauth/core';
 
 @Page({
   templateUrl: 'build/pages/login/login.html'
@@ -15,7 +18,13 @@ export class LoginPage {
 	}
 	
 	login() {
-		this.nav.push(HomePage);
+		this.cordovaOauth = new CordovaOauth(new Google({clientId: "", appScope: ["email"]}));
+        this.cordovaOauth.login().then((success) => {
+                console.log(JSON.stringify(success));
+            }, (error) => {
+                console.log(error);
+            });
+		//this.nav.push(HomePage);
 	}
 
 }
